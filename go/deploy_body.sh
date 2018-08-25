401715b10d3aacd20ceee294ef88e15c9d922dc1
catatsuy <catatsuy@catatsuy.org>
2018-08-25 14:41:10 +0900

    add deploy script

---
+++ go/deploy_body.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+echo "start deploy"
+GOPATH=$PWD GOOS=linux go build -v isubata
+for server in isu; do
+    ssh -t $server "sudo systemctl stop isubata.golang.service"
+    scp ./isubata $server:/home/isucon/isubata/webapp/go/isubata
+    rsync -av ./src/isubata/views/ $server:/home/isucon/isubata/webapp/go/src/isubata/views/
+    ssh -t $server "sudo systemctl start isubata.golang.service"
+done
+
+echo "finish deploy"
